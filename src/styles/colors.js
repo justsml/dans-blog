f06def2e4c556e8f9dc998444030942f27ee120f
Dan Levy <397632+justsml@users.noreply.github.com>
2020-03-05 10:44:28 -0700

    fixing accent color (#56)

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,7 +1,7 @@
 module.exports = {
   background: "#ffffff",
   white: "#ffffff",
-  accent: "#578c18e6", //"#709425",
+  accent: "rgba(87, 140, 24, 0.9)", //"#709425",
   bright: "#ffffff",
   dark: "#333333",
   gray: "#555555",
