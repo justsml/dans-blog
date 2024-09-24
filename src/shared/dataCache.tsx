806f35067acf158c502f8470ff9ebbf6d94d0c9a
Dan Levy <justsml@gmail.com>
2024-09-24 15:59:42 -0600

    pre-publish

--- src/shared/dataCache.tsx
+++ src/shared/dataCache.tsx
@@ -104,13 +104,13 @@ export const PostCollections = {
   /** Popular posts according to google analytics. 2024/Q2 */
   getPopularPosts() {
     return [
+      "breaking-unicorns",
       "js-quiz-14-date-time-questions-test-your-knowledge",
       "javascript-promises-quiz",
       "contribute-to-open-source-the-easy-way",
-      "the-4-pillars-of-collaborative-culture",
-      "you-may-not-need-axios",
-      "should-you-use-named-or-default-exports",
-      // "naming-things-real-good",
+      "naming-things-real-good",
+      // "you-may-not-need-axios",
+      // "should-you-use-named-or-default-exports",
     ].map((slug) => PostCollections._postsBySlug[slug]);
   },
 
