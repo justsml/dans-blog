d75a0cc0e79020bf7689f29c3d88f0c3110c805a
Dan Levy <justsml@gmail.com>
2024-12-11 22:53:00 -0700

    Add readme for plugins, implement reading time plugin, and update styles with new icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,21 +7,54 @@ import pagefind from "astro-pagefind";
 
 import expressiveCode from "astro-expressive-code";
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
-// import { rehypeHeadingIds } from '@astrojs/markdown-remark'
-// import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import { rehypeHeadingIds } from '@astrojs/markdown-remark'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+// import { h } from "hastscript";
+// import { toString } from "hast-util-to-string";
+// import autolinkHeadings from "rehype-autolink-headings";
+// import rehypeSlug from 'rehype-slug'
+// import { autolinkConfig } from "./src/plugins/rehype-autolink-config";
+
+// The following configuration for rehype-autolink-headings was taken from https://github.com/withastro/docs/blob/main/astro.config.ts
+// const AnchorLinkIcon = h(
+//   "svg",
+//   {
+//     width: 16,
+//     height: 16,
+//     version: 1.1,
+//     viewBox: "0 0 16 16",
+//     xlmns: "http://www.w3.org/2000/svg",
+//   },
+//   h("path", {
+//     fillRule: "evenodd",
+//     fill: "currentcolor",
+//     d: "M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z",
+//   }),
+// );
+
+// const createSROnlyLabel = (text) => {
+//   const node = h("span.sr-only", `Section titled ${escape(text)}`);
+//   node.properties["is:raw"] = true;
+//   return node;
+// };
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false
+  },
   output: "static",
   trailingSlash: "ignore",
   build: {
+    inlineStylesheets: "auto",
+    concurrency: 10,
+    // assets: "_assets",
     format: "directory",
   },
   cacheDir: ".cache",
   site: "https://danlevy.net",
-  markdown: {
-  },
-  
+  markdown: {},
+
   integrations: [
     pagefind(),
     react({}),
@@ -31,9 +64,62 @@ export default defineConfig({
       plugins: ["line-numbers"],
     }),
     mdx({
-      // rehypePlugins: [rehypeHeadingIds, rehypeAutolinkHeadings],
+      // extendMarkdownConfig: true,
+      // rehypePlugins: {
+      //   extends: [
+      //     [
+      //       autolinkHeadings,
+      //       {
+      //         behavior: "append",
+      //         group: ({ tagName }) =>
+      //           h(`div.heading-wrapper.level-${tagName}`, {
+      //             tabIndex: -1,
+      //           }),
+      //         content: (heading) => [
+      //           h(
+      //             `span.anchor-icon`,
+      //             {
+      //               ariaHidden: "true",
+      //             },
+      //             AnchorLinkIcon,
+      //           ),
+      //           createSROnlyLabel(toString(heading)),
+      //         ],
+      //       },
+      //     ],
+      //   ],
+      // },
 
-    }),
+      // Ref: https://jan-mueller.at/blog/next-level-heading-anchors/
+
+      rehypePlugins: [rehypeHeadingIds, rehypeAutolinkHeadings],
+      // {
+
+        // extends: [
+        //   [
+        //     rehypeHeadingIds,
+        //     {},
+        //     rehypeAutolinkHeadings,
+        //     {
+        //       behavior: "append",
+        //       // group: ({ tagName }) =>
+        //       //   h(`div.heading-wrapper.level-${tagName}`, {
+        //       //     tabIndex: -1,
+        //       //   }),
+        //       // content: (heading) => [
+        //       //   h(
+        //       //     `span.anchor-icon`,
+        //       //     {
+        //       //       ariaHidden: "true",
+        //       //     },
+        //       //     AnchorLinkIcon,
+        //       //   ),
+        //       //   createSROnlyLabel(toString(heading)),
+        //       // ],
+        //     },
+        //   ],
+        // ],
+      }),
     sitemap(),
     tailwind({
       applyBaseStyles: false,
@@ -41,6 +127,26 @@ export default defineConfig({
     }),
     // partytown(),
   ],
+
+  // markdown: {
+  //   gfm: true,
+  //   // remarkPlugins: [remarkReadingTime],
+  //   rehypePlugins: [
+  //     rehypeSlug,
+  //     [autolinkHeadings, autolinkConfig],
+  //     // [
+  //     //   rehypeExternalLinks,
+  //     //   {
+  //     //     target: '_blank',
+  //     //     rel: 'noreferrer',
+  //     //   },
+  //     // ],
+  //   ],
+  //   // shikiConfig: {
+  //   //   theme: 'monokai',
+  //   // },
+  // },
+
   plugins: [pluginLineNumbers()],
   image: {
     service: {
