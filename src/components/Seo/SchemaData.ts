ae57282f020567125707e83f4e66b8cda0fabc3d
Dan Levy <justsml@gmail.com>
2024-12-29 11:52:16 -0700

    update build, added json LD graph data

---
+++ src/components/Seo/SchemaData.ts
@@ -0,0 +1,129 @@
+
+export const SiteAuthors = {
+  justsml: {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    image: "https://danlevy.net/images/avatar.webp",
+    name: "Dan Levy",
+    url: "https://danlevy.net/",
+    disambiguatingDescription:
+      "Entrepreneur, Engineer, Educator, and Current World's Only Dan.best",
+    sameAs: [
+      "https://github.com/justsml",
+      "https://twitter.com/justsml",
+      "https://www.linkedin.com/in/realdanlevy/",
+      "https://bsky.app/profile/danlevy.net",
+      "https://mastodon.social/@justsml",
+    ],
+    jobTitle: "Entrepreneur, Engineer, Educator",
+    worksFor: {
+      "@type": "Organization",
+      name: "Hotel Engine",
+    },
+    alumniOf: {
+      "@type": "EducationalOrganization",
+      name: "University of Denver",
+      address: {
+        "@type": "PostalAddress",
+        addressLocality: "Denver",
+        addressRegion: "CO",
+        addressCountry: "USA",
+      },
+    },
+    knowsAbout: [
+      "Tech Leadership",
+      "Team Building",
+      "Software Architecture",
+      "Security by Design",
+      "System Design",
+      "Cloud Infrastructure",
+      "Open Source Software",
+      "LLM and GPT Engineering",
+      "Curriculum Design",
+      "Speech Writing",
+      "Startup Consulting",
+      "Investor Relations",
+    ],
+    hasOccupation: [
+      {
+        "@type": "Occupation",
+        name: "Sr. Engineering Manager",
+        employer: {
+          "@type": "Organization",
+          name: "Hotel Engine",
+        },
+      },
+      {
+        "@type": "Occupation",
+        name: "Principal Engineer",
+        employer: {
+          "@type": "Organization",
+          name: "Hotel Engine",
+        },
+      },
+      {
+        "@type": "Occupation",
+        name: "Engineering Manager",
+        employer: {
+          "@type": "Organization",
+          name: "Hotel Engine",
+        },
+      },
+      {
+        "@type": "Occupation",
+        name: "Sr. Lead Engineer",
+        employer: {
+          "@type": "Organization",
+          name: "Quizlet",
+        },
+      },
+      {
+        "@type": "Occupation",
+        name: "Web Program Director",
+        employer: {
+          "@type": "Organization",
+          name: "Lambda School",
+        },
+      },
+      {
+        "@type": "Occupation",
+        name: "Lead Instructor",
+        employer: {
+          "@type": "Organization",
+          name: "Galvanize",
+        },
+      },
+    ],
+    hasPOS: [
+      {
+        "@type": "Product",
+        name: "Functional Promises",
+        url: "https://fpromises.io/",
+      },
+      {
+        "@type": "Product",
+        name: "DataAnalyzer.app",
+        url: "https://dataanalyzer.app/",
+      },
+    ],
+    skills: [
+      "JavaScript/TypeScript/NodeJS",
+      "React/React Native",
+      "Docker",
+      "Postgres/PostGIS",
+      "Kubernetes",
+      "Neural Networks",
+      "Security Analysis",
+      "LLM & GPT",
+      "Rust",
+      "System Design",
+      "Cloud Architecture",
+    ],
+    workLocation: {
+      "@type": "PostalAddress",
+      addressLocality: "Denver",
+      addressRegion: "CO",
+      addressCountry: "USA",
+    },
+  },
+};
